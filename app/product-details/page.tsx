840b439855232e80bee536d919f368e00940f1aa
pallab99 <kitkatpallab@gmail.com>
2023-06-30 19:05:13 +0600

    Added product details page

---
+++ app/product-details/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from '@/Components/Navbar';
+import Product_Details from '../../Components/Product-Details';
+
+export default function Page() {
+  return <>
+    <Navbar />
+    <Product_Details />
+  </>;
+}
