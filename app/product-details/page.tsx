4c763ece3a206e4e7055a209064af796c0a9b8d9
pallab99 <kitkatpallab@gmail.com>
2023-07-01 17:28:02 +0600

    Added dynamic product details page

--- app/product-details/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '@/Components/Navbar';
-import Product_Details from '../../Components/Product-Details';
-
-export default function Page() {
-  return <>
-    <Navbar />
-    <Product_Details />
-  </>;
-}
